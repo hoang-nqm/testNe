7c8149b8ac0aa04a2f89c775505ea830014ec1bb
minhhoang4520 <hoangnqmse141014@fpt.edu.vn>
2022-05-20 23:41:18 +0700

    oki

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Test thử nha",
+    introTitle: 'Phương Thảo',
+    introDesc: `Ngày mai em có rảnh có chán thì alo là anh có mặt đi chơi lummm`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đang rất buồn chán vào ngày cúi tuần 🥰',
+    desc: 'Phải chăng em rất muốn đi chơi ???? ',
+    btnYes: 'Đúm lummm ',
+    btnNo: 'Không nha :3',
+    question: 'Trong tuần có bao nhiêu ngày sao cuối tuần lại không đi chơi',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Đi thì đi không đi thì đi <33333333',
+    mess: 'Anh biết mà đi thoiiiii 😘😘',
+    messDesc: 'Tối mai 5h, anh qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100006447619293' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
